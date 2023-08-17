760b202ff7af1fcd0011de9bf445aca770e3229c
Jens Heine <binbash@gmx.net>
2023-08-17 08:56:28 +0200

    Update merge_generator.sql

--- merge_generator.sql
+++ merge_generator.sql
@@ -3,10 +3,17 @@
 20230809 jens heine, nils loewemann, yetis batmaz
 */
 
-/* MAN MUSS NUR DIESE BEIDEN VARIABLEN SETZEN (und "result to text" im ssms anklicken)!!! */
-declare @target_table_schema as varchar(255) = 'schema'
-declare @target_table_name   as varchar(255) = 'table'
+/* 
+Man muss nur 2 Variablen setzen (und "result to text" im ssms anklicken)!!! 
+Dann das Ergebniss dieses ganzen SQL Statements noch ein wenig anpassen (sieht Punkt (1)
+im Output des SQL's.
+*/
+
+
 /* MAN MUSS NUR DIESE BEIDEN VARIABLEN SETZEN!!! */
+declare @target_table_schema as varchar(255) = 'schema_name'
+declare @target_table_name   as varchar(255) = 'table_name'
+
 
 
 
@@ -19,6 +26,7 @@ from
 select 'Bitte manuell das SQL für die Quelltabelle hier einfügen unter (1)' as c, 1 as nr
 union
 select ' ' as c, 2 as nr
+
 union
 
 select CONCAT('MERGE ', @target_table_schema, '.', @target_table_name,' as TARGET ', 
@@ -55,13 +63,12 @@ C.COLUMN_NAME not in ('Create_Date', 'Last_Update_Date')
 
 union
 
-select CONCAT('from', CHAR(13)
-, ' <BASETABLE(s)_NAME or JOINS> as S', CHAR(13)
-, ')', 
+select CONCAT('from', 
+CHAR(13), ' <BASETABLE(s)_NAME or JOINS> as S', 
+CHAR(13), ')', 
 CHAR(13), 'AS SOURCE ON ', 
 CHAR(13), '( ') as c,
 10 as nr
-
 
 union
 
@@ -97,8 +104,7 @@ from(
 select
 CONCAT(
  ' TARGET.', '[', TARGET_TABLE_COLUMNS.COLUMN_NAME, ']' , ' <> SOURCE.', '[', TARGET_TABLE_COLUMNS.COLUMN_NAME, ']' 
-,CHAR(13), '    OR (TARGET.', '[', TARGET_TABLE_COLUMNS.COLUMN_NAME, ']' , ' IS NULL AND SOURCE.', '[', TARGET_TABLE_COLUMNS.COLUMN_NAME, ']' 
-,' IS NOT NULL)', 
+,CHAR(13), '    OR (TARGET.', '[', TARGET_TABLE_COLUMNS.COLUMN_NAME, ']' , ' IS NULL AND SOURCE.', '[', TARGET_TABLE_COLUMNS.COLUMN_NAME, ']' ,' IS NOT NULL)', 
 CHAR(13), '    OR (TARGET.', '[', TARGET_TABLE_COLUMNS.COLUMN_NAME, ']' , ' IS NOT NULL AND SOURCE.', '[', TARGET_TABLE_COLUMNS.COLUMN_NAME, ']' , ' IS NULL)'
 ) as c,
 20 as nr,
@@ -132,9 +138,7 @@ CHAR(13), '  Last_Update_Date = getdate() ') as c, 30 as nr
 
 union
 
-
 /* 2. iterationssache (update-block) */
-
 
 select
 CONCAT(
@@ -173,7 +177,6 @@ C.TABLE_NAME = @target_table_name
 )
 ) as TARGET_TABLE_COLUMNS
 
-
 union
 
 select
@@ -184,11 +187,10 @@ CHAR(13), '  Create_Date ',
 CHAR(13), ' ,Last_Update_Date ') as c,
 50 as nr
 
-
 union
 
-
 /* insert block */
+
 select
 CONCAT(
  ' ,', '[', TARGET_TABLE_COLUMNS.COLUMN_NAME, ']'  
@@ -212,7 +214,6 @@ and
 C.COLUMN_NAME not in ('Create_Date', 'Last_Update_Date')
 ) as TARGET_TABLE_COLUMNS
 
-
 union
 
 select concat(') ', 
@@ -221,12 +222,10 @@ CHAR(13), '(',
 CHAR(13), '  getdate() ', 
 CHAR(13), ' ,getdate() ') as c, 70 as nr
 
-
-
 /* values block */
 
-
 union
+
 select
 CONCAT(
  ' ,SOURCE.', '[', TARGET_TABLE_COLUMNS.COLUMN_NAME, ']' 
@@ -250,9 +249,7 @@ and
 C.COLUMN_NAME not in ('Create_Date', 'Last_Update_Date')
 ) as TARGET_TABLE_COLUMNS
 
-
 union
-
 
 select  '); ' as c, 90 as nr
 
